2818f3fa7c7134c5f430f45e3f09beeeb316162c
Pedro Lucas <lucasdasilvajunior@yahoo.com.br>
2016-10-03 01:52:18 -0300

    Removed anonymous scope from MainController

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,26 +1,23 @@
-(() => {
-  'use strict';
+'use strict';
 
-  const Controller = require('./controller');
-  const NotificationView = require('../library/notification-view');
-  const ToolbarView = require('../components/toolbar-view');
-  const util = require('../library/util');
-  const $ = require("jquery");
+const Controller = require('./controller');
+const NotificationView = require('../library/notification-view');
+const ToolbarView = require('../components/toolbar-view');
+const util = require('../library/util');
+const $ = require("jquery");
 
-  class MainController extends Controller {
+class MainController extends Controller {
 
-    constructor() {
-      super($('.main'));
-      this.initialize();
-    }
-
-    initialize() {
-      this.toolbarView = new ToolbarView();
-      this.addComponent(this.toolbarView);
-    }
-
+  constructor() {
+    super($('.main'));
+    this.initialize();
   }
 
-  window.rootController = new MainController();
+  initialize() {
+    this.toolbarView = new ToolbarView();
+    this.addComponent(this.toolbarView);
+  }
 
-})();
+}
+
+window.rootController = new MainController();
